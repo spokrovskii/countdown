516fc12731be2626c6349f15651816a733bd948c
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-18 14:44:36 -0400

    created controller, views and routes for goals

---
+++ db/migrate/20160718174832_create_goals.rb
@@ -0,0 +1,11 @@
+class CreateGoals < ActiveRecord::Migration
+  def change
+    create_table :goals do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.timestamp :created_at
+      t.timestamp :due_time
+      t.integer :unser_id, null: false
+    end
+  end
+end
