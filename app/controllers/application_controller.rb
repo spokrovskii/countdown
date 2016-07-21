4ac17047b9f77ff83d8b5b8c875105943e436edc
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-21 14:09:49 -0400

    messed up, it is going to be deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,41 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:user_first_name, :user_last_name]
+    keys = [
+      { roles: [] },
+      :user_first_name,
+      :user_last_name,
+      :email,
+      :current_password,
+      :password,
+      :password_confirmation,
+      :avatar
+    ]
+
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(keys)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(keys)
+    end
+  end
+
+  def check_permissions
+    item = model.find(params[:id])
+
+    if !current_user || !current_user.can_modify?(item)
+      flash[:alert] = 'You do not have permission to complete that action.'
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def model
+    self.class.name.sub("Controller", "").singularize.constantize
   end
 end
