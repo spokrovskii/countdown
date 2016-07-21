e70d36c76a5fc833fa6f20ebb5da6253870d078a
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-21 12:03:10 -0400

    fixed hound violations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-
   devise_for :users
-    root 'homepage#index'
 
-    resources :homepage, only: [:index]
+  root 'homepage#index'
 
-    resources :admin_users, only: [:index, :destroy]
+  resources :homepage, only: [:index]
+
+  resources :admin_users, only: [:index, :destroy]
 
   resources :goals, only: [:index, :show]
 end
