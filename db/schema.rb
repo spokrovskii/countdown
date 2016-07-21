83e5bf34606577bdb6adc36a2c9d8688a8dcbb75
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-21 14:43:26 -0400

    trying to revert back, deleting extra stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721144029) do
+ActiveRecord::Schema.define(version: 20160721131259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,12 +37,9 @@ ActiveRecord::Schema.define(version: 20160721144029) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "user_first_name",        default: "", null: false
-    t.string   "user_last_name",         default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["user_first_name", "user_last_name"], name: "index_users_on_user_first_name_and_user_last_name", using: :btree
 
 end
