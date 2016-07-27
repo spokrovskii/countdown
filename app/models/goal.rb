01f9fedfbbf23882b7fffc7610591b210453c0c0
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-27 10:45:23 -0400

    hound violations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -9,18 +9,18 @@ class Goal < ActiveRecord::Base
      diff = time_finished - time_started
      days = diff / 60 / 60 / 24
      days_number = days.floor
-     hours_fraction = (days-days.floor)
+     hours_fraction = (days - days.floor)
      hours = hours_fraction * 24
      hours_number = hours.floor
-     minutes_fraction = (hours-hours_number)
+     minutes_fraction = (hours - hours_number)
      minutes = minutes_fraction * 60
      minutes_number = minutes.floor
-     seconds_fraction = (minutes-minutes_number)
+     seconds_fraction = (minutes - minutes_number)
      seconds = seconds_fraction * 60
      seconds_number = seconds.floor
-      answer = days_number.to_s + ' day(s) ' +
-               hours_number.to_s + ' hour(s) ' +
-               minutes_number.to_s + ' minute(s) ' +
-               seconds_number.to_s + ' seconds'
-  end
+     days_number.to_s + ' day(s) ' +
+     hours_number.to_s + ' hour(s) ' +
+     minutes_number.to_s + ' minute(s) ' +
+     seconds_number.to_s + ' seconds'
+end
 end
