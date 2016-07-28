26fe369d2c2291d24092eff5897e7650d4f95e65
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-28 12:17:47 -0400

    added feature tests

---
+++ spec/features/goals/view_all_goals_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'View all goals' do
+  let(:current_user) { FactoryGirl.create(:user) }
+
+  before do
+   login_user(current_user)
+
+    end
+  scenario 'When user sing ins see all goals that belongs to that user.' do
+    goals = FactoryGirl.create_list(:goal, 3, user: current_user)
+    visit goals_path
+
+    expect(page).to have_content('Goals')
+    goals.each do |goal|
+      expect(page).to have_content(goal.name)
+    end
+  end
+end
---
+++ spec/features/goals/view_goal_details_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'View details of the goal for the current user' do
+  let(:current_user) { FactoryGirl.create(:user) }
+
+  before do
+     login_user(current_user)
+   end
+
+  scenario 'When a user clicks on the goals name se goal details.' do
+    goal =  FactoryGirl.create(:goal, user: current_user)
+    visit goals_path
+
+    click_link(goal.name)
+
+    expect(page).to have_content(goal.name)
+    expect(page).to have_content(goal.description)
+    expect(page).to have_content(goal.due_time.strftime('%a, %d %b %Y %H:%M:%S'))
+    expect(page).to have_content(goal.amount_of_time_left_to_finish_goal)
+  end
+end
