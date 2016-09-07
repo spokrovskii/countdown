b694bc9a69d44847cf925249e3c4a06833cec36d
Serge Pokrovskii <spokrovskey@gmail.com>
2016-09-07 17:29:12 -0600

    added style and changes home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
+devise_scope :user do
+  authenticated :user do
+    root 'goals#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
+
   root 'homepage#index'
 
   resources :homepage, only: [:index]
