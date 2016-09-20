db499abb0e07017b2295302fd7ed27fe590ad30c
Serge Pokrovskii <spokrovskey@gmail.com>
2016-09-20 12:18:33 -0600

    Adjustng migrations for heroku

--- db/migrate/20160910230118_add_name_to_subtasks.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToSubtasks < ActiveRecord::Migration
-  def change
-    add_column :subtasks, :name, :string
-  end
-end
--- db/migrate/20160920181158_create_subtasks.rb
+++ db/migrate/20160920181158_create_subtasks.rb
@@ -2,6 +2,8 @@ class CreateSubtasks < ActiveRecord::Migration
   def change
     create_table :subtasks do |t|
       t.integer :goal_id
+      t.string :name
+      t.integer :user_id
     end
   end
 end
